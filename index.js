5b0c124b188bdc96c2620c722bf200a4db406c2e
Sparkenstein <zetabytes.pp@gmail.com>
2024-11-13 12:42:04 +0530

    cleanup

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ import "dotenv/config";
 import { connection } from "./db/connection.js";
 import express from "express";
 import authMiddleware from "./middlewares/auth.middleware.js";
-
-import authRouter from "./routes/auth.middleware.js";
+import cors from "cors";
+import authRouter from "./routes/auth.routes.js";
 import bookmarkRouter from "./routes/bookmark.routes.js";
 
 // constants
+
 const app = express();
 const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 // middlewares and routes
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "https://bookmark-frontend-q6u2.vercel.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use("/auth", authRouter);
 
 app.use("/bookmarks", authMiddleware, bookmarkRouter);
